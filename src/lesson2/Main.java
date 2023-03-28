72ac115b73973d413128fe030959d495511c5900
AndreiParmon <parmonandrei@gmail.com>
2023-03-28 22:43:56 +0300

    Initial commit

---
+++ src/lesson2/Main.java
@@ -0,0 +1,20 @@
+package lesson2;
+
+import com.sun.source.util.SourcePositions;
+import lesson2.task.Task1;
+import lesson2.task.Task4;
+import lesson2.task.Task5;
+
+public class Main {
+    public static void main(String[] args) {
+//        для Task1
+//        System.out.println(Task1.printString(10, '+', '/'));
+
+//        для Task4
+//        Task4.writeInFile();
+
+//        для Task5
+        Task5.write(".");
+
+    }
+}
